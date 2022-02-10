cee32e577baa1862a08db8a98aeef2605e26d7cf
deviniverson <iverson.devin7@gmail.com>
2022-02-10 22:29:21 +0000

    added functionality

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+# Script Name: helloworld.sh
+# Description of purpose: returns a message 
+# Example usage: to confirm that I have access to files in this directory
+# Expected output: I am working perfectly
+# Declaration of variables (if any): N/A
+# Declaration of functions (if any): N/A
+
+# Main
+echo "I am working perfectly"
+
+# End
