406fe8f36f9587abed195620c607abb4b9b56aba
deviniverson <iverson.devin7@gmail.com>
2022-02-16 03:53:58 +0000

    added comments

---
+++ Class4-Arrays/array-function.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Script Name: array-function.sh
+# Description of purpose: 
+# Example usage: 
+# Example output: 
+# Declaration of variables (if any): 
+# Declaration of functions (if any): 
+
+# Main
+
+# End
