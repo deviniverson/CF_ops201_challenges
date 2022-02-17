100cc785bce1167917c6f4902f3c5458e3f2f2bb
deviniverson <iverson.devin7@gmail.com>
2022-02-17 22:19:29 +0000

    assignment completed

--- Class4-Arrays/array-function.sh
+++ Class4-Arrays/array-function.sh
@@ -8,5 +8,24 @@
 # Declaration of functions (if any): 
 
 # Main
+# make each directory
+mkdir dir1
+mkdir dir2
+mkdir dir3
+mkdir dir4
+
+# combine the directories into an array 
+dirs=(dir1 dir2 dir3 dir4)
+
+# test: print name of all directories in array
+echo ${dirs[*]}
+
+# for loop to create a new file in each directory
+for d in ${dirs[@]}
+do
+    cd $d
+    touch newfile.txt
+    cd ..
+done
 
 # End
