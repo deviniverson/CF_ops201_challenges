a6c68243b7f96b2e11317139377da4bbbf7a84ab
deviniverson <iverson.devin7@gmail.com>
2022-02-17 22:26:12 +0000

    completed comments

--- Class4-Arrays/array-function.sh
+++ Class4-Arrays/array-function.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
 # Script Name: array-function.sh
-# Description of purpose: 
+# Description of purpose: make four directories, combine all of them into an array then put a .txt file in each directory
 # Example usage: 
-# Example output: 
-# Declaration of variables (if any): 
-# Declaration of functions (if any): 
+# Example output: four directories, each with their own newfile.txt
+# Declaration of variables (if any): dirs
+# Declaration of functions (if any): none, could if I wanted to ask for user input on how many directories to create.
 
 # Main
 # make each directory
@@ -20,11 +20,14 @@ dirs=(dir1 dir2 dir3 dir4)
 # test: print name of all directories in array
 echo ${dirs[*]}
 
-# for loop to create a new file in each directory
+# for loop to create a new file in each directory, "@" means all
 for d in ${dirs[@]}
 do
+    # move into the disired directory
     cd $d
+    # create file
     touch newfile.txt
+    # step back to starting point
     cd ..
 done
 
