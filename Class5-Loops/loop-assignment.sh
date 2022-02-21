d236dcaf96041f377a52c447350da2438c111ec2
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-02-21 14:57:05 -0800

    started assignment

---
+++ Class5-Loops/loop-assignment.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+# Script Name: loop-assignment.sh
+# Description of purpose: 
+# Example usage: 
+# Example output: 
+# Declaration of variables (if any): 
+# Declaration of functions (if any): 
+
+# Main
+
+
+
+# End
