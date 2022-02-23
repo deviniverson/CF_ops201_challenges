0c641d4d0b6977f8e8e86b8675505528da218fee
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-02-22 20:35:28 -0800

    add Class6 and file-dir-checker

---
+++ Class6-Conditional/file-dir-checker.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# Script Name: file-dir-checker.sh
+# Description of purpose: 
+# Example usage: 
+# Example output: 
+# Declaration of variables (if any): 
+# Declaration of functions (if any): 
+
+# Main
+
+# while loop for user input
+while [ : ]
+do
+
+done
+# End
