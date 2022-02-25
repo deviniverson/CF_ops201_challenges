0ebd904b7a62f0fcd95cb83b98fb367018b38728
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-02-24 22:38:46 -0800

    tweaked wording, tried to include sleep commands to control time between prompts

--- Class6-Conditional/file-dir-checker.sh
+++ Class6-Conditional/file-dir-checker.sh
@@ -4,7 +4,7 @@
 # Description of purpose: creates file or directory if the name is unique
 # Example usage: 
 # Example output: 
-# Declaration of variables (if any): file, input_arr, dir, file_answer, dir_answer, file-dir
+# Declaration of variables (if any): file, input_arr, dir, file_answer, dir_answer, user_choice
 # Declaration of functions (if any): menu, create_dir, create_file, check_dir, check_file
 
 # Main
@@ -13,6 +13,7 @@ input_arr=()
 
 # function to check if file exists
 check_file(){
+    
     if [[ -f $file ]]
     then
         echo "$file already exists"   
@@ -24,6 +25,7 @@ check_file(){
 
 # function to check if directory exists
 check_dir(){
+
     if [[ -d $dir ]]
     then
         echo "$dir already exists"   
@@ -36,7 +38,7 @@ check_dir(){
 # function to prompt user to confirm that they want to create a file and to create it
 create_file(){
 
-    read -p "Do you want to create a new file with the name of $file in the current directory? (y for yes, n for no, e to exit) " file_answer 
+    read -p "Create a new file with the name of $file in the current directory? (y for yes, n for no, e to exit) " file_answer 
     
     if [ $file_answer == y ] || [ $file_answer == Y ]
     then
@@ -54,7 +56,7 @@ create_file(){
 }
 # function to prompt user to confirm that they want to create a new directory and to create it
 create_dir(){
-    read -p "Do you want to create a new directory with the name of $dir in the current directory? (y for yes, n for no, e to exit) " dir_answer 
+    read -p "Create a new directory with the name of $dir in the current directory? (y for yes, n for no, e to exit) " dir_answer 
     
     if [ $dir_answer == y ] || [ $dir_answer == Y ]
     then
@@ -75,18 +77,18 @@ create_dir(){
 menu(){
     while [ : ]
     do
-        read -p "Do you want to create a new file or directory? (f for file, d for directory, e to exit) " user_choice
+        read -p "Create a new file or directory? (f for file, d for directory, e to exit) " user_choice
         
         if [ $user_choice == f ] || [ $user_choice == F ]
         then
-            read -p "What would you like to name your new file? (name must be unique from other files of your filesystem) " file
+            read -p "Name your new file? (name must be unique from other files of your filesystem) " file
             input_arr+=($file)
             
             check_file
         
         elif [ $user_choice == d ] || [ $user_choice == D ]
         then
-            read -p "What would you like to name your new directory? (name must be unique from other directories of you filesystem) " dir
+            read -p "Name your new directory? (name must be unique from other directories of you filesystem) " dir
             input_arr+=($dir)
             
             check_dir
