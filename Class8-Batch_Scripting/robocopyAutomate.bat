c0f7b17d72ac964a4adfd91f15a974b4a0be2a98
deviniverson <iverson.devin7@gmail.com>
2022-03-04 03:40:31 +0000

    added comments to code

--- Class8-Batch_Scripting/robocopyAutomate.bat
+++ Class8-Batch_Scripting/robocopyAutomate.bat
@@ -5,6 +5,7 @@
 
 @echo off
 
+:: robocopy command to copy the MyProjectFiles folder and put the copies into the BackupFolder. Also a log is created showing all files that were copied each time script is ran.
 ROBOCOPY C:\Users\j.thompson\Desktop\MyProjectFiles E:\BackupFolder /E /LOG+:E:\copylog.log
 
 Log File : E:\copylog.log
