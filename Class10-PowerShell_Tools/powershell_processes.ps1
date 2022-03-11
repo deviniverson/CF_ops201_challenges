dd9588ca6fb08acb436b578e37cd0a4c0b952a5e
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-03-10 20:15:54 -0800

    added main function and main menu for user

--- Class10-PowerShell_Tools/powershell_processes.ps1
+++ Class10-PowerShell_Tools/powershell_processes.ps1
@@ -4,15 +4,15 @@
 # Purpose: Useful tools for use in powershell for future reference.
 
 # Function that prints all active processes ordered by highest CPU time comsuption at top
-Function ordered_cpu 
+Function Sorted_CPU 
 {
     Get-Process | Sort-Object CPU -desc
 }
 
 # Function that prints all active processes ordered by highest Process Identification Number at top
-Function Sorted-Ids 
+Function Sorted_Ids 
 {
-    # 
+    
     Get-Process | Sort-Object Id -desc
 }
 
@@ -20,7 +20,7 @@ Function Sorted-Ids
 Function TopFiveWorkingSets
 {
 
-    Get-Process | Sort-Object -Property WS | Select -Object -Last 5
+    Get-Process | Sort-Object -Property WS -desc | Select -First 5
 }
 
 # Opens Internet explorer and goes to website 
@@ -51,4 +51,63 @@ Function KillById($id)
 {
 
     Stop-Process -Id $id -Confirm -PassThru
+}
+
+# Main function to provide user experience and the ability to choose function to use
+Function main 
+{
+    while ($val -ne 3)
+    {
+        $res | Read-Host "Main Menu 'n Enter the number of function to run: 'n 1.Sorted_CPU 'n 2.Sorted_Ids 'n 3.TopFiveWorkingSets 'n 4.OwaspExplorer 'n 5.LoopOwaspExplorer 'n 6.ExitAllExplorers 'n 7.KillById 'n 8.Exit 'n Function #: "
+        
+        if($res -eq 1)
+        {
+            $val++
+            Sorted_CPU
+        }
+        elseif($res -eq 2)
+        {
+            $val++
+            Sorted_Ids
+        }
+        elseif($res -eq 3)
+        {
+            $val++
+            TopFiveWorkingSets
+        }
+        elseif($res -eq 4)
+        {
+            $val++
+            OwaspExplorer
+        }
+        elseif($res -eq 5)
+        {
+            $val++
+            write-host(" ")
+            $l | Read-Host "How many tabs of OwaspExplorer would you like? "
+            LoopOwaspExplorer($l)
+        }
+        elseif($res -eq 6)
+        {
+            $val++
+            ExitAllExplorers
+        }
+        elseif($res -eq 7)
+        {
+            $val++
+            write-host(" ")
+            $id | Read-Host "ID of process to be killed: "
+            KillById($id)
+        }
+        elseif($res -eq 8)
+        {
+            Break
+        }
+        else {
+            $val++
+            continue
+        }
+    }
+
+    write-host("Exiting Script")
 }
