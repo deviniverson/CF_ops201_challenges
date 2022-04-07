ad3f34b3690a510993bbfe9792aa27a84b6745ed
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-04-06 19:13:45 -0700

    enable smb1

--- Class11-PowerShell-automate/custom_config.ps1
+++ Class11-PowerShell-automate/custom_config.ps1
@@ -37,12 +37,12 @@ Function Hyper_On
 
 }
 
-# Disable SMBv1, an insecure protocol
-#Function Disable-SMBv1
-#{
-#    Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
+# Enable SMBv1, an insecure protocol
+Function Enable-SMBv1
+{
+    Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
 
-#}
+}
 
 # main function
 Function Main
@@ -52,7 +52,7 @@ Function Main
     Remote_Manage
     Remove-bloatware
     Hyper_On
-    #Disable-SMBv1
+    Enable-SMBv1
     # one restart at end to cover all bases
     Restart-Computer
 }
